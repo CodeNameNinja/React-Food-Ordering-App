4fced5e42facd03d0c2c53893823247f99a64b76
Mitchell Yuen <mitchell.yuen@lavalamp.biz>
2021-06-21 18:49:40 +0200

    add basic markup for Header Component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,18 @@
-import React from 'react';
-
-const Header = () => {
-
-}
+import React, { Fragment } from "react";
+import classes from "./Header.module.css";
+import mealsImage from  'assets/meals.jpeg';
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+          <h1>React Meals</h1>
+          <button>Cart</button>
+      </header>
+      <div>
+          <img src={mealsImage} alt="A Table full of delicious food" />
+      </div>
+    </Fragment>
+  );
+};
 
 export default Header;
