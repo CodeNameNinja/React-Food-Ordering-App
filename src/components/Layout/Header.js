3095531c50b197efbed87b75782714ac10ac2c38
Mitchell Yuen <mitchell.yuen@lavalamp.biz>
2021-06-21 18:51:54 +0200

    feat(styling): add basic styling to header component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
           <h1>React Meals</h1>
           <button>Cart</button>
       </header>
-      <div>
+      <div className={classes['main-image']}>
           <img src={mealsImage} alt="A Table full of delicious food" />
       </div>
     </Fragment>
