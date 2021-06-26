a8baec0fb41ab057d143289b704a8f7c7eb8f687
Mitchell Yuen <mitchell.yuen@lavalamp.biz>
2021-06-26 13:50:22 +0200

    feat: add functionality to open and close cart modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import "./App.css";
 import Header from "components/Layout/Header";
 import Meals from "components/Meals/Meals";
 import Cart from "components/Cart/Cart"
 function App() {
+  const [cartIsShown, setCartIsShow] = useState(false);
+  function showCartHandler(){
+    setCartIsShow(true);
+  }
+  function hideCartHandler(){
+    setCartIsShow(false);
+  }
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,8 +2,8 @@ import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 
-const Backdrop = () => {
-  return <div className={classes.backdrop} />;
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
 
 const ModalOverlay = (props) => {
@@ -18,7 +18,7 @@ const Modal = (props) => {
     const portalElement =  document.getElementById("overlays");
   return (
     <Fragment>
-      {ReactDOM.createPortal( <Backdrop />, portalElement )}
+      {ReactDOM.createPortal( <Backdrop onClose={props.onClose}/>, portalElement )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
